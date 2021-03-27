268b8b790311e4165726a6a93442fa525974af68
强 <oncwnuGZ7aiQpSybzcK6TnLAgkjI@git.weixin.qq.com>
2021-03-27 18:59:20 +0300

    1

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,7 +9,17 @@ class CfgNotifyForm(FlaskForm):
     account_month   = DateTimeField("月份", format='%Y-%m',default = datetime.utcnow)
     account_type    = SelectField('收支类型', choices=[('Z', '支出'), ('S', '收入')],
                                validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
-    account_name    = StringField('支付项目', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
+    account_name    = StringField('支付项目' )#, choices=[
+                                            # ('外卖', '外卖'),
+                                            # ('打车', '打车'),
+                                            # ('咖啡', '咖啡'),
+                                            # ('超市', '超市'),
+                                            # ('地铁', '地铁'),
+                                            # ('游戏', '游戏'),
+                                            # ('水', '水'),
+                                            # ('购物', '购物'),
+                                            #
+                                            # ])
     account_money   = StringField('金钱', )
     status          = BooleanField('生效标识', default=True)
     account_date    = DateTimeField("时间", format='%Y-%m-%d',default = datetime.utcnow)
